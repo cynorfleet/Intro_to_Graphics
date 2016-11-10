f0e6f021f32fc299c4b033b9bd59e8cdfc355f88
Christian Norfleet <cynorfleet@gmail.com>
2016-11-09 22:06:13 -0600

    added data structure documentation added reset function

--- Project0/Project0/Object.h
+++ Project0/Project0/Object.h
@@ -24,44 +24,68 @@ struct bounding
 		x_min(xmin), x_max(xmax), y_min(ymin), y_max(ymax), z_min(zmin), z_max(zmax) {}
 
 	//	Find vec4 min/max Constructor
-	void operator()(const vec4 &other)
-	/*-------------------------------------------- bounding -----
+	void operator()(const vec4 &vertex_element)
+	/*-------------------------------------------- bounding ------------
 	|  Function 	bounding()
 	|
-	|  Purpose: 	This function will take in a vec4 and check if it 
-	|				contains x/y/z min/max values and store it in bounding.
+	|  Purpose: 	This will overload the parentheses operator, 
+	|				read in a vec4, and check if it contains x/y/z
+	|				min/max values storing it in bounding data structure.
 	|
 	|  Parameters:
-	|	other (IN) -- ARG DESCRIPTION HERE
+	|	vertex_element (IN) -- An element of the vertices array (vec4)
 	|
 	|  Returns:  	N/A
 	*-------------------------------------------------------------------*/
 	{
 		//	Loop the x/y/z values of vec4
-		for(int i = 0; i < 3; i++)
+		for(int axis = 0; axis < 3; axis++)
 		{
 			/***********************************************************************
 			*	use *(&x_min) as index zero which ref x_min. Add i to traverse (i.e. 
 			*	i=1 would traverse to [address space of x_min + 1] == y_min). 
 			************************************************************************/
-			if (other[i] < *(&x_min + i))
-				*(&x_min + i) = other[i];
+			if (vertex_element[axis] < *(&x_min + axis))
+				*(&x_min + axis) = vertex_element[axis];
 			/***********************************************************************
 			*	use *(&x_max) as index three which ref x_max. Add i to traverse (i.e.
 			*	i=0 would traverse to [address space of x_max + 0] == x_max).
 			************************************************************************/
-			else if (other[i] > *(&x_max + i) )
-				*(&x_max + i) = other[i];
+			else if (vertex_element[axis] > *(&x_max + axis) )
+				*(&x_max + axis) = vertex_element[axis];
 		}
 	}
 
+	void Reset()
+	/*-------------------------------------------- Reset -----
+	|  Function 	Reset()
+	|
+	|  Purpose: 	Resets the bounding data
+	|
+	|  Parameters:
+	|	 (IN) -- ARG DESCRIPTION HERE
+	|
+	|  Returns:  	N/A
+	*-------------------------------------------------------------------*/
+	{ x_min = 0, x_max = 0, y_min = 0, y_max = 0, z_min = 0, z_max = 0; }
+
 	string ToString()
+	/*-------------------------------------------- ToString ------------
+	|  Function 	ToString()
+	|
+	|  Purpose: 	Returns the Min/Max values of X/Y/Z in a vec4
+	|
+	|  Parameters:
+	|	 (IN) -- ARG DESCRIPTION HERE
+	|
+	|  Returns:  	String
+	*-------------------------------------------------------------------*/
 	{
-		return	"\n***********************************************************\n"
+		return	"----------------------------------------\n"
 				"X_Min = " + to_string(x_min) + "\tX_Max = " + to_string(x_max) + '\n' +
 				"Y_Min = " + to_string(y_min) + "\tY_Max = " + to_string(y_max) + '\n' +
 				"Z_Min = " + to_string(z_min) + "\tZ_Max = " + to_string(z_max) + '\n' +
-				"***********************************************************\n";
+				"----------------------------------------\n\n";
 	}
 
 	//  --- Indexing Operator ---
