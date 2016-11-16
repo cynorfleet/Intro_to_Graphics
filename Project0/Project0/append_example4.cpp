cc611fc8635c669509dddae9483f71abf842e7b1
Christian Norfleet <kumin@PHILLY-PC>
2016-11-15 22:53:08 -0600

    working on rotate

--- Project0/Project0/append_example4.cpp
+++ Project0/Project0/append_example4.cpp
@@ -9,21 +9,28 @@
 #include "Angel.h"
 #include "Object.h"
 
-
 #pragma comment(lib, "freeglut")
 #pragma comment(lib, "glew32")
 
 // Array of rotation angles (in degrees) for each coordinate axis
-enum { Xaxis = 0, Yaxis = 1, Zaxis = 2, NumAxes = 3 };
-int      Axis = Xaxis;
-GLfloat  Theta[NumAxes] = { 0.0, 0.0, 0.0 };
+enum axisType { X = 0, Y = 1, Z = 2 };
+axisType axis = X;
+int axisActive = axis;
 
+GLfloat Theta[3] = { 0.0, 0.0, 0.0 };
+GLuint modelViewLoc;
+GLuint program;
 GLuint  theta;  // The location of the "theta" shader uniform variable
-vector<string> modelname = {"cube.obj", "bb8.obj", "megatron.obj", "batman.obj", "ironmanmarkII.obj"};
+vec4 at, up;
+vec4 camera(0.0, 0.0, 5.0, 1);
+
+GLuint ProjectionLoc;
+mat4 projection;
+GLfloat projection_near, projection_far, aspectRatio;
+
+vector<string> modelname = { "cube.obj", "bb8.obj", "megatron.obj", "batman.obj", "ironmanmarkII.obj" };
 vector <Object> model;
 int activemodel = 0;
-// Load shaders and use the resulting shader program
-	GLuint program;
 //----------------------------------------------------------------------------
 
 //----------------------------------------------------------------------------
@@ -32,14 +39,21 @@ int activemodel = 0;
 void
 init()
 {
-	// Create a vertex array object
-	GLuint vao;
-	glGenVertexArrays(1, &vao);
-	glBindVertexArray(vao);
-
 	program = InitShader("vshader_a4.glsl", "fshader_a4.glsl");
 
 	glUseProgram(program);
+	const int size = model.size();
+	int const num = size;
+	// Create a vertex array object
+	GLuint** vao = new GLuint*[model.size()];
+
+	glGenVertexArrays(model.size, vao[model.size()]);
+
+	for (int i = 0; i < model.size(); i++)
+	{
+		glBindVertexArray(*vao[i]);
+		model[i].load(program);
+	}
 
 	model[activemodel].load(program);
 
@@ -49,11 +63,42 @@ init()
 	glClearColor(1.0, 1.0, 1.0, 1.0);
 }
 
+mat4 _RotateFunc(GLfloat angle, int which_axis)
+{
+	switch (which_axis)
+	{
+	case 0:
+		return mat4(1.0, 0.0, 0.0, 0.0,
+			0.0, cos(theta), sin(theta), 0.0,
+			0.0, -sin(theta), cos(theta), 0.0,
+			0.0, 0.0, 0.0, 1.0);
+		break;
+	case 1:
+		return mat4(cos(theta), 0.0, sin(theta), 0.0,
+			0.0, 1.0, 0.0, 0.0,
+			-sin(theta), 0.0, cos(theta), 0.0,
+			0.0, 0.0, 0.0, 1.0);
+		break;
+	case 2:
+		return mat4(cos(theta), -sin(theta), 0.0, 0.0,
+			sin(theta), cos(theta), 0.0, 0.0,
+			0.0, 0.0, 1.0, 0.0,
+			0.0, 0.0, 0.0, 1.0);
+		break;
+	}
+}
+
 //----------------------------------------------------------------------------
 int frame, fps, time, timebase = 0;
 void
 display(void)
 {
+	mat4 modelview;
+	mat4 rotate = _RotateFunc(Theta[X], X) * _RotateFunc(Theta[Y], Y)
+		*_RotateFunc(Theta[Z], Z);
+
+	at = model[activemodel].;
+
 	string winowname = model[activemodel].meshname + ": FPS:%d ";
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
@@ -73,8 +118,6 @@ display(void)
 	}
 
 	glutSwapBuffers();
-
-
 }
 
 //----------------------------------------------------------------------------
@@ -85,8 +128,7 @@ keyboard(unsigned char key, int x, int y)
 {
 	switch (key) {
 	case 's': case 'S':
-		activemodel < model.size() -1 ? activemodel++ : activemodel = 0;
-		model[activemodel].load(program);
+		activemodel < model.size() - 1 ? activemodel++ : activemodel = 0;
 		model[activemodel].draw();
 		break;
 	case 'w':
@@ -132,14 +174,14 @@ idle(void)
 
 void
 _LoadModels()
-	/*-------------------------------------------- _LoadModels -----
-	|  Function 	_LoadModels()
-	|
-	|  Purpose: 	Will populate vector with model objects generated from
-	|				the names provided in model names.
-	|
-	|  Returns:  	N/A
-	*-------------------------------------------------------------------*/
+/*-------------------------------------------- _LoadModels -----
+|  Function 	_LoadModels()
+|
+|  Purpose: 	Will populate vector with model objects generated from
+|				the names provided in model names.
+|
+|  Returns:  	N/A
+*-------------------------------------------------------------------*/
 {
 	if (model.size() == 0)
 		for (int i = 0; i < modelname.size(); i++)
